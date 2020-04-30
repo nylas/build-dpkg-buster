ff33315e87c981dafbc40a2936ea41cc570796c9
David Baumgold <david@davidbaumgold.com>
2020-04-30 20:45:40 +0200

    non-interactive

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-mk-build-deps --install debian/control
+# Set the install command to be used by mk-build-deps (use --yes for non-interactive)
+install_tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes"
+# Install build dependencies automatically
+mk-build-deps --install --tool="${install_tool}" debian/control
+# Build the package
 dpkg-buildpackage $@
