34014b918a197e2f32b5422f37237d9e7aed9d4b
David Baumgold <david@davidbaumgold.com>
2020-05-03 13:11:09 +0200

    set -e

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 # Set the install command to be used by mk-build-deps (use --yes for non-interactive)
 install_tool="apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends --yes"
 # Install build dependencies automatically
